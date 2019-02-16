9da6c34f8bf90c7a490f0d8ca7151a1348d39dab
lizhi chen <382256780@qq.com>
2019-02-16 21:09:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# durid-datasource
+add durid datasource
+
+1.查看服务健康情况详情
+management.endpoint.health.show-details=always
+http://127.0.0.1:8080/actuator/health
+
+2.查看应用介绍
+http://127.0.0.1:8080/actuator/info
+
+3.查看spring context中的beans
+http://127.0.0.1:8080/actuator/beans
+
+3.配置durid连接池
+
---
+++ src/main/java/com/springboot/DuridDatasourceApplication.java
@@ -0,0 +1,62 @@
+package com.springboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * CommandLineRunner spring-boot启动后要执行的操作
+ */
+@SpringBootApplication
+@Slf4j
+public class DuridDatasourceApplication implements CommandLineRunner{
+
+	/**
+	 * 通过DataSourceAutoConfiguration配置
+     */
+	@Autowired
+	private DataSource dataSource;
+
+	/**
+	 * 通过JdbcTemplateAutoConfiguration配置
+     */
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	/**
+	 * 通过DataSourceTransactionManagerAutoConfiguration配置
+     */
+	@Autowired
+	private DataSourceTransactionManager dataSourceTransactionManager;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuridDatasourceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		log.info("dataSource={}",dataSource);
+		showConnection();
+		showData();
+	}
+
+	private void showData() {
+		jdbcTemplate.queryForList("SELECT * FROM currency")
+				.forEach(row -> log.info(row.toString()));
+	}
+
+	private void showConnection() throws SQLException {
+		Connection conn = dataSource.getConnection();
+		log.info("conn={}",conn.toString());
+		conn.close();
+	}
+}
+
